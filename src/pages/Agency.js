88ba9d94bf1946a9bc1ddee39aa7a22e041b0eba
Paul Gialis <paulmg@uw.edu>
2024-05-31 16:27:54 -0700

    location and agency changes

--- src/pages/Agency.js
+++ src/pages/Agency.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import _ from 'lodash';
-import './team.css'
+import './team.css';
+import { getFirestore, doc, updateDoc } from 'firebase/firestore';
 
-export default function Agency({ data }) {
+export default function Agency({ data, authenticatedUser }) {
   console.log('Agency data being passed on: ', data);
+  console.log('Authenticated user: ', authenticatedUser);
   const [sortByCriteria, setSortByCriteria] = useState(null);
   const [isAscending, setIsAscending] = useState(null);
   const [selectedPosition, setSelectedPosition] = useState('');
+  const [filterByLocation, setFilterByLocation] = useState(false);
 
   const handleClick = (event) => {
     const { name } = event.currentTarget;
@@ -31,6 +33,10 @@ export default function Agency({ data }) {
     setSelectedPosition(event.target.value);
   };
 
+  const handleLocationFilterChange = () => {
+    setFilterByLocation(!filterByLocation);
+  };
+
   let sortedData = [...data];
   if (sortByCriteria) {
     sortedData = _.sortBy(sortedData, sortByCriteria);
@@ -39,145 +45,176 @@ export default function Agency({ data }) {
     }
   }
 
-  const filteredData = sortedData.filter((user) => user.team === null && (selectedPosition === '' || user.position === selectedPosition));
+  const filteredData = sortedData.filter((user) => {
+    if (filterByLocation && authenticatedUser && authenticatedUser.state && authenticatedUser.county) {
+      return (
+        user.team === null &&
+        (user.position === selectedPosition || selectedPosition === '') &&
+        user.state === authenticatedUser.state &&
+        user.county === authenticatedUser.county
+      );
+    } else {
+      return user.team === null && (user.position === selectedPosition || selectedPosition === '');
+    }
+  });
+
+  const handleAddToTeam = async (user) => {
+    if (!authenticatedUser || !authenticatedUser.team) {
+      console.error('Authenticated user does not have a team.');
+      return;
+    }
+
+    try {
+      const db = getFirestore();
+      const userRef = doc(db, "Ball", user.username);
+      await updateDoc(userRef, { team: authenticatedUser.team });
+      console.log(`User ${user.username} has been added to team ${authenticatedUser.team}`);
+    } catch (error) {
+      console.error('Error adding user to team: ', error);
+    }
+  };
 
   return (
     <div className='team'>
-    <div className="table-responsive">
-      <div className="filter-container">
-        <label htmlFor="position-filter">Filter by Position:</label>
-        <select id="position-filter" value={selectedPosition} onChange={handlePositionChange}>
-          <option value="">None</option>
-          <option value="PG">PG</option>
-          <option value="SG">SG</option>
-          <option value="SF">SF</option>
-          <option value="PF">PF</option>
-          <option value="C">C</option>
-        </select>
-      </div>
-      <table className="table w-100">
-        <thead>
-          <tr>
-            <th className="text-center">
-              Name
-            </th>
-            <th className="text-center">
-              Email
-            </th>
-            <th className="text-center">
-              Position
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="height"
-                active={sortByCriteria === 'height'}
-                ascending={sortByCriteria === 'height' && isAscending}
-                onClick={handleClick}
-              />
-              Height
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="weight"
-                active={sortByCriteria === 'weight'}
-                ascending={sortByCriteria === 'weight' && isAscending}
-                onClick={handleClick}
-              />
-              Weight
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="wingspan"
-                active={sortByCriteria === 'wingspan'}
-                ascending={sortByCriteria === 'wingspan' && isAscending}
-                onClick={handleClick}
-              />
-              Wingspan
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="points"
-                active={sortByCriteria === 'points'}
-                ascending={sortByCriteria === 'points' && isAscending}
-                onClick={handleClick}
-              />
-              PPG
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="assists"
-                active={sortByCriteria === 'assists'}
-                ascending={sortByCriteria === 'assists' && isAscending}
-                onClick={handleClick}
-              />
-              AST
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="rebounds"
-                active={sortByCriteria === 'rebounds'}
-                ascending={sortByCriteria === 'rebounds' && isAscending}
-                onClick={handleClick}
-              />
-              REB
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="steals"
-                active={sortByCriteria === 'steals'}
-                ascending={sortByCriteria === 'steals' && isAscending}
-                onClick={handleClick}
-              />
-              SPG
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="blocks"
-                active={sortByCriteria === 'blocks'}
-                ascending={sortByCriteria === 'blocks' && isAscending}
-                onClick={handleClick}
-              />
-              BLK
-            </th>
-            <th className="text-center">
-              <SortButton
-                name="games"
-                active={sortByCriteria === 'games'}
-                ascending={sortByCriteria === 'games' && isAscending}
-                onClick={handleClick}
-              />
-              Games
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {filteredData.map((profileObj) => (
-            <tr key={profileObj.username}>
-              <td>{`${profileObj.firstName} ${profileObj.lastName}`}</td>
-              <td className="text-center">{profileObj.email}</td>
-              <td className="text-center">{profileObj.position}</td>
-              <td className="text-center">{profileObj.height}</td>
-              <td className="text-center">{profileObj.weight}</td>
-              <td className="text-center">{profileObj.wingspan}</td>
-              <td className="text-center">{(profileObj.points / profileObj.games).toFixed(1)}</td>
-              <td className="text-center">{(profileObj.assists / profileObj.games).toFixed(1)}</td>
-              <td className="text-center">{(profileObj.rebounds / profileObj.games).toFixed(1)}</td>
-              <td className="text-center">{(profileObj.steals / profileObj.games).toFixed(1)}</td>
-              <td className="text-center">{(profileObj.blocks / profileObj.games).toFixed(1)}</td>
-              <td className="text-center">{profileObj.games}</td>
-              <td className="text-center">
-                <Link to={`/profile/${profileObj.username}`}>
-                  <span className="material-icons">add</span>
-                </Link>
-              </td>
+      <div className="table-responsive">
+        <div className="filter-container">
+          <label htmlFor="position-filter">Filter by Position:</label>
+          <select id="position-filter" value={selectedPosition} onChange={handlePositionChange}>
+            <option value="">None</option>
+            <option value="PG">PG</option>
+            <option value="SG">SG</option>
+            <option value="SF">SF</option>
+            <option value="PF">PF</option>
+            <option value="C">C</option>
+          </select>
+          <div>
+            <input
+              type="checkbox"
+              id="location-filter"
+              checked={filterByLocation}
+              onChange={handleLocationFilterChange}
+            />
+            <label htmlFor="location-filter">Filter by Location</label>
+          </div>
+        </div>
+        <table className="table w-100">
+          <thead>
+            <tr>
+              <th className="text-center">Name</th>
+              <th className="text-center">Email</th>
+              <th className="text-center">Position</th>
+              <th className="text-center">
+                <SortButton
+                  name="height"
+                  active={sortByCriteria === 'height'}
+                  ascending={sortByCriteria === 'height' && isAscending}
+                  onClick={handleClick}
+                />
+                Height
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="weight"
+                  active={sortByCriteria === 'weight'}
+                  ascending={sortByCriteria === 'weight' && isAscending}
+                  onClick={handleClick}
+                />
+                Weight
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="wingspan"
+                  active={sortByCriteria === 'wingspan'}
+                  ascending={sortByCriteria === 'wingspan' && isAscending}
+                  onClick={handleClick}
+                />
+                Wingspan
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="points"
+                  active={sortByCriteria === 'points'}
+                  ascending={sortByCriteria === 'points' && isAscending}
+                  onClick={handleClick}
+                />
+                PPG
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="assists"
+                  active={sortByCriteria === 'assists'}
+                  ascending={sortByCriteria === 'assists' && isAscending}
+                  onClick={handleClick}
+                />
+                AST
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="rebounds"
+                  active={sortByCriteria === 'rebounds'}
+                  ascending={sortByCriteria === 'rebounds' && isAscending}
+                  onClick={handleClick}
+                />
+                REB
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="steals"
+                  active={sortByCriteria === 'steals'}
+                  ascending={sortByCriteria === 'steals' && isAscending}
+                  onClick={handleClick}
+                />
+                STL
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="blocks"
+                  active={sortByCriteria === 'blocks'}
+                  ascending={sortByCriteria === 'blocks' && isAscending}
+                  onClick={handleClick}
+                />
+                BLK
+              </th>
+              <th className="text-center">
+                <SortButton
+                  name="games"
+                  active={sortByCriteria === 'games'}
+                  ascending={sortByCriteria === 'games' && isAscending}
+                  onClick={handleClick}
+                />
+                Games
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {filteredData.map((profileObj) => (
+              <tr key={profileObj.username}>
+                <td>{`${profileObj.firstName} ${profileObj.lastName}`}</td>
+                <td className="text-center">{profileObj.email}</td>
+                <td className="text-center">{profileObj.position}</td>
+                <td className="text-center">{profileObj.height}</td>
+                <td className="text-center">{profileObj.weight}</td>
+                <td className="text-center">{profileObj.wingspan}</td>
+                <td className="text-center">{(profileObj.points / profileObj.games).toFixed(1)}</td>
+                <td className="text-center">{(profileObj.assists / profileObj.games).toFixed(1)}</td>
+                <td className="text-center">{(profileObj.rebounds / profileObj.games).toFixed(1)}</td>
+                <td className="text-center">{(profileObj.steals / profileObj.games).toFixed(1)}</td>
+                <td className="text-center">{(profileObj.blocks / profileObj.games).toFixed(1)}</td>
+                <td className="text-center">{profileObj.games}</td>
+                {authenticatedUser && authenticatedUser.team && (
+                  <td className="text-center">
+                    <button onClick={() => handleAddToTeam(profileObj)}>
+                      <span className="material-icons">add</span>
+                    </button>
+                  </td>
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
-  
 }
 
 function SortButton(props) {
